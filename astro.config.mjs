386f11e86e3f856f8a3ded72776b1fad47fb15de
Rich Kelly <rich@richkelly.uk>
2025-02-04 15:16:50 +0000

    Updated settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,23 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://shopify-astro-headless-storefront.netlify.app',
+	base: './',
+	output: 'static',
+	srcDir: './src',
+	publicDir: './public',
+	outDir: './dist',
+	cacheDir: './cache',
+	compressHTML: false,
+	build: {
+		format: 'directory',
+		assets: 'dist',
+		inlineStylesheets: 'always'
+	},
+	image: {
+	  remotePatterns: [{}],
+	  domains: []
+	},
+	integrations: []
+});
